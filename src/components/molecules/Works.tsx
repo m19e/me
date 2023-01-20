8a85c88daf1277931b7959184242f691ba61a40c
m19e <nexus.me7290@gmail.com>
2023-01-20 17:40:36 +0900

    feat: simple accordion

--- src/components/molecules/Works.tsx
+++ src/components/molecules/Works.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable tailwindcss/migration-from-tailwind-2 */
 import Image from "next/image"
+import { useState } from "react"
 
 import type { WorkContent } from "types"
 import { Sections } from "components/molecules/Sections"
@@ -18,6 +19,7 @@ export const Works = ({ contents }: Props) => {
 
 const Work = ({ content }: { content: WorkContent }) => {
   const { title, sections } = content
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
@@ -28,7 +30,10 @@ const Work = ({ content }: { content: WorkContent }) => {
             src="https://placehold.jp/900x420.png"
             alt="mock"
           />
-          <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-300">
+          <div
+            className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-300"
+            onClick={() => setIsOpen(true)}
+          >
             <div className="flex items-center px-10 h-full">
               <p className="font-sans font-thin text-white">{title}</p>
             </div>
@@ -36,24 +41,30 @@ const Work = ({ content }: { content: WorkContent }) => {
         </div>
       </div>
 
-      <div className="flex justify-center px-8 pb-12 w-full bg-zinc-50 md:px-auto">
+      <div
+        className={`flex justify-center px-8 pb-12 w-full bg-zinc-50 md:px-auto ${
+          isOpen ? "" : "hidden"
+        }`}
+      >
         <div className="space-y-8 w-full md:w-[900px]">
           <div className="">
             <div className="flex justify-end">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={0.2}
-                stroke="currentColor"
-                className="w-20 h-20"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <button onClick={() => setIsOpen(false)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={0.2}
+                  stroke="currentColor"
+                  className="w-20 h-20"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
             </div>
             <h2 className="text-3xl italic font-thin">{title}</h2>
           </div>
