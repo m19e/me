b78c174820b70f33db2c262520228224ce0f5706
m19e <nexus.me7290@gmail.com>
2023-01-20 22:07:17 +0900

    feat: auto-scroll when toggle detail open

--- src/components/molecules/Works.tsx
+++ src/components/molecules/Works.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable tailwindcss/migration-from-tailwind-2 */
 import { motion, useAnimationControls } from "framer-motion"
 import Image from "next/image"
+import { Link as Scroll } from "react-scroll"
 
 import type { WorkContent } from "types"
 import { Sections } from "components/molecules/Sections"
@@ -10,14 +11,18 @@ interface Props {
 }
 
 export const Works = ({ contents }: Props) => {
-  const works = contents.map((c) => <Work key={c.title} content={c} />)
+  const works = contents.map((c, i) => (
+    <Work key={c.title} content={c} last={contents.length === i + 1} />
+  ))
 
   return (
-    <div className="flex flex-col gap-4 items-center min-h-screen">{works}</div>
+    <div className="flex flex-col gap-4 items-center py-8 min-h-screen">
+      {works}
+    </div>
   )
 }
 
-const Work = ({ content }: { content: WorkContent }) => {
+const Work = ({ content, last }: { content: WorkContent; last: boolean }) => {
   const { title, sections } = content
   const controls = useAnimationControls()
 
@@ -32,6 +37,9 @@ const Work = ({ content }: { content: WorkContent }) => {
     })
   }
 
+  const summaryID = `summary-${title}`
+  const detailID = `detail-${title}`
+
   return (
     <>
       <div className="flex justify-center px-8 w-full md:px-auto">
@@ -41,18 +49,22 @@ const Work = ({ content }: { content: WorkContent }) => {
             src="https://placehold.jp/900x420.png"
             alt="mock"
           />
-          <div
-            className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-300"
-            onClick={openDetail}
-          >
-            <div className="flex items-center px-10 h-full">
-              <p className="font-sans font-thin text-white">{title}</p>
+          <Scroll to={`detail-${title}`} duration={last ? 1000 : 500} smooth>
+            <div
+              id={summaryID}
+              className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-300"
+              onClick={openDetail}
+            >
+              <div className="flex items-center px-10 h-full">
+                <p className="font-sans font-thin text-white">{title}</p>
+              </div>
             </div>
-          </div>
+          </Scroll>
         </div>
       </div>
 
       <motion.div
+        id={detailID}
         className="overflow-hidden pt-1 w-full"
         initial={{ height: "4px" }}
         animate={controls}
@@ -62,22 +74,24 @@ const Work = ({ content }: { content: WorkContent }) => {
           <div className="space-y-8 w-full md:w-[900px]">
             <div className="">
               <div className="flex justify-end">
-                <button onClick={closeDetail}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={0.2}
-                    stroke="currentColor"
-                    className="w-20 h-20"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+                <Scroll to={summaryID} duration={1000} smooth>
+                  <button onClick={closeDetail}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={0.2}
+                      stroke="currentColor"
+                      className="w-20 h-20"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </Scroll>
               </div>
               <h2 className="text-3xl italic font-thin">{title}</h2>
             </div>
