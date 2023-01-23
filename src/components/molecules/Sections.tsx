e473cdb498238b2ebaf302f55eeba32e6a4e2e64
m19e <nexus.me7290@gmail.com>
2023-01-23 17:59:19 +0900

    style: remove un-used class

--- src/components/molecules/Sections.tsx
+++ src/components/molecules/Sections.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Sections = ({ contents }: Props) => {
   const sections = contents.map(({ title, text }) => (
     <div key={title[0]} className="space-y-2 font-latego">
-      <h3 className="text-sm md:text-base tracking-wid">{title[0]}</h3>
+      <h3 className="text-sm md:text-base">{title[0]}</h3>
       <p className="text-xs whitespace-pre-wrap md:text-sm">{text}</p>
     </div>
   ))
