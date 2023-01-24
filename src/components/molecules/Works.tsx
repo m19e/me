ac63d3973177d7962c702d2de37acbe37bb03127
m19e <nexus.me7290@gmail.com>
2023-01-24 23:48:46 +0900

    refactor: replace svg by Icon

--- src/components/molecules/Works.tsx
+++ src/components/molecules/Works.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Link as Scroll } from "react-scroll"
 
 import type { WorkContent } from "types"
+import { Icon } from "components/atoms/Icon"
 import { Sections } from "components/molecules/Sections"
 
 interface Props {
@@ -93,20 +94,7 @@ const Work = ({ content, last }: { content: WorkContent; last: boolean }) => {
               <div className="flex justify-end">
                 <Scroll to={summaryID} duration={1000} smooth>
                   <button onClick={closeDetail} className="w-20 h-20">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={0.2}
-                      stroke="currentColor"
-                      className="w-full h-full"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
+                    <Icon type="x" />
                   </button>
                 </Scroll>
               </div>
@@ -128,29 +116,10 @@ const Work = ({ content, last }: { content: WorkContent; last: boolean }) => {
               </div>
               <div className="flex gap-2 px-2">
                 <a href="" className="w-6 h-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="w-full h-full fill-current"
-                  >
-                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                  </svg>
+                  <Icon type="github" />
                 </a>
                 <a href="" className="w-6 h-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-full h-full"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                    />
-                  </svg>
+                  <Icon type="link" />
                 </a>
               </div>
             </div>
@@ -170,16 +139,13 @@ const Footer = () => {
         <p className="text-base font-latego">©2023 m19e</p>
       </div>
       <div className="flex gap-2">
-        <a href="https://github.com/m19e" target="_blank" rel="noreferrer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="fill-current"
-          >
-            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-          </svg>
+        <a
+          className="w-6 h-6"
+          href="https://github.com/m19e"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icon type="github" />
         </a>
       </div>
     </footer>
