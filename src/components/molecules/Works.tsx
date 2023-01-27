e3300d524fbd8b85399d228f338473cadd8dee21
m19e <nexus.me7290@gmail.com>
2023-01-27 18:24:27 +0900

    style: set font-rounded on hero

--- src/components/molecules/Works.tsx
+++ src/components/molecules/Works.tsx
@@ -29,9 +29,9 @@ const Hero = () => {
   return (
     <div className="flex justify-center px-8 w-full md:px-auto">
       <div className="aspect-video flex flex-col justify-center w-full md:w-[900px]">
-        <h1 className="space-y-4">
-          <span className="font-latego text-6xl font-thin">m19e.me</span>
-          <div className="flex flex-col px-1 text-xl italic font-thin leading-6">
+        <h1 className="space-y-4 font-rounded font-light">
+          <span className="text-6xl">m19e.me</span>
+          <div className="flex flex-col px-1 text-xl italic leading-6">
             <span>PORTFOLIO OF m19e</span>
             <span>FRONTEND DEVELOPER</span>
           </div>
