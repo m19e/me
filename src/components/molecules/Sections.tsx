f1e2f32127fa81af7aa3bac22f192a3205c1a2de
m19e <nexus.me7290@gmail.com>
2023-01-27 19:15:21 +0900

    style: font-rounded at sections

--- src/components/molecules/Sections.tsx
+++ src/components/molecules/Sections.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export const Sections = ({ contents }: Props) => {
   const sections = contents.map(({ title, text }) => (
-    <div key={title[0]} className="space-y-2 font-latego">
+    <div key={title[0]} className="space-y-2">
       <h3 className="text-sm md:text-base">{title[0]}</h3>
       <p className="text-xs whitespace-pre-wrap md:text-sm">{text}</p>
     </div>
   ))
 
-  return <div className="space-y-4">{sections}</div>
+  return <div className="space-y-4 font-rounded">{sections}</div>
 }
