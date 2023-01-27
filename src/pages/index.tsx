8f968f243ed581107fb677ee4dfe74a471dcd671
m19e <nexus.me7290@gmail.com>
2023-01-27 21:42:16 +0900

    refactor: simplify props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,16 @@ import { Works } from "components/molecules/Works"
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-const Page: NextPage<Props> = ({ contents }) => {
-  return <Works contents={contents} />
+const Page: NextPage<Props> = (props) => {
+  return <Works {...props} />
 }
 
 export const getStaticProps = async () => {
-  const works = await Client.getList<WorkContent>({ endpoint: "works" })
+  const { contents } = await Client.getList<WorkContent>({ endpoint: "works" })
 
   return {
     props: {
-      contents: works.contents,
+      contents,
     },
   }
 }
