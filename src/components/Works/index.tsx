1ae4d49699881fd9bedfc36d7696d31d0e6dfdd3
m19e <nexus.me7290@gmail.com>
2023-01-28 20:13:19 +0900

    refactor: extract hero from works

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -8,6 +8,8 @@ import type { DynamicImage } from "types/image"
 import { Icon } from "components/atoms/Icon"
 import { Sections } from "components/molecules/Sections"
 
+import { Hero } from "./Hero"
+
 interface Props {
   contents: WorkContent[]
   images: DynamicImage[][]
@@ -28,22 +30,6 @@ export const Works = ({ contents, images }: Props) => {
       <Hero />
       {works}
       <Footer />
-    </div>
-  )
-}
-
-const Hero = () => {
-  return (
-    <div className="flex justify-center px-8 w-full md:px-auto">
-      <div className="aspect-video flex flex-col justify-center w-full md:w-[900px]">
-        <h1 className="space-y-4 font-rounded font-light">
-          <span className="text-6xl">m19e.me</span>
-          <div className="flex flex-col px-1 text-xl italic leading-6">
-            <span>PORTFOLIO OF m19e</span>
-            <span>FRONTEND DEVELOPER</span>
-          </div>
-        </h1>
-      </div>
     </div>
   )
 }
