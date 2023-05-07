a2f5882af04733c782eecf1949174788dd676468
m19e <nexus.me7290@gmail.com>
2023-05-07 17:53:41 +0900

    feat: fix header title

--- src/components/Works/Hero.tsx
+++ src/components/Works/Hero.tsx
@@ -3,7 +3,7 @@ export const Hero = () => {
     <div className="flex justify-center px-8 w-full md:px-auto">
       <div className="aspect-video flex flex-col justify-center w-full md:w-[900px]">
         <h1 className="space-y-4 font-rounded font-light">
-          <span className="text-6xl">m19e.me</span>
+          <span className="text-6xl">m19e/me</span>
           <div className="flex flex-col px-1 text-xl italic leading-6">
             <span>PORTFOLIO OF m19e</span>
             <span>FRONTEND DEVELOPER</span>
